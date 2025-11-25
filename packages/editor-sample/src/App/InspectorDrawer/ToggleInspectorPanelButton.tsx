f481cfc9864f46dba752de4dbd06368e2f210b2e
Adem <112782893+adeemmm23@users.noreply.github.com>
2025-11-25 21:54:54 +0100

    Refactor ToggleInspectorPanelButton and ToggleSamplesPanelButton to include Tooltip for better accessibility and user experience

--- packages/editor-sample/src/App/InspectorDrawer/ToggleInspectorPanelButton.tsx
+++ packages/editor-sample/src/App/InspectorDrawer/ToggleInspectorPanelButton.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
 
-import { AppRegistrationOutlined, LastPageOutlined } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import { LastPageOutlined, TuneOutlined } from '@mui/icons-material';
+import { IconButton, Tooltip } from '@mui/material';
 
 import { toggleInspectorDrawerOpen, useInspectorDrawerOpen } from '../../documents/editor/EditorContext';
 
 export default function ToggleInspectorPanelButton() {
   const inspectorDrawerOpen = useInspectorDrawerOpen();
 
-  const handleClick = () => {
-    toggleInspectorDrawerOpen();
-  };
-  if (inspectorDrawerOpen) {
-    return (
-      <IconButton onClick={handleClick}>
-        <LastPageOutlined fontSize="small" />
-      </IconButton>
-    );
-  }
   return (
-    <IconButton onClick={handleClick}>
-      <AppRegistrationOutlined fontSize="small" />
-    </IconButton>
+    <Tooltip title={inspectorDrawerOpen ? 'Close Inspector Panel' : 'Open Inspector Panel'}>
+      <IconButton onClick={toggleInspectorDrawerOpen}>
+        {inspectorDrawerOpen ? <LastPageOutlined fontSize="small" /> : <TuneOutlined fontSize="small" />}
+      </IconButton>
+    </Tooltip>
   );
 }
